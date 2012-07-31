fc5a9bccc8059ac189ca1f0704f473470c018ab9
nborkowska <nborkowska@gmail.com>
2012-08-01 00:59:03 +0200

    option parser

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 import math
 import numpy as np
+import re
+import sys
+
+from optparse import OptionParser
 
 class RotationMatrix(np.ndarray):
 
@@ -15,7 +19,7 @@ class Rotation(object):
     def __init__(self, point):
         self.point = point
     
-    def rotate(self):           #i to powinna byc metoda mikrostanu, no ale niekoniecznie bo to moze sobie obracac macierz po prostu, moze tu nie bedzie spr czy dozwolone 
+    def rotate(self):
         pass
 
 
@@ -35,7 +39,8 @@ class R270(Rotation):
 
 
 class Aminoacid(object):
-
+    """ ??? """
+    
     HYDROPHOBIC = 'H'
     POLAR = 'P'
 
@@ -46,9 +51,17 @@ class Aminoacid(object):
         return self.symbol == POLAR
 
 
+class Chain(object):
+
+    def __init__(self, sequence):
+        if not re.match('[HP]+$', sequence):
+            raise ValueError("Sequence should consist of 'H' and 'P' characters")
+        self.sequence = sequence
+
+
 class Microstate(object):
 
-    sequence = ''
+    chain = ''
     
     def __init__(self, coords, energy=10000):
         self.coords = coords
@@ -59,3 +72,26 @@ class Microstate(object):
 
     def transform(self):
         pass
+
+
+def main():
+    
+    parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
+
+    parser.add_option('-s', '--sequence', dest='sequence', \
+            help='protein sequence, ex. "HPHPHPHPPHPH"')
+    parser.add_option('-i', '--maximum', type='float', dest='tMax', \
+            default=1.0, help='initial temperature')
+    parser.add_option('-f', '--minimum', type='float', dest='tMin', \
+            default=0.15, help='final temperature')
+    parser.add_option('-d', '--delta', type='float', dest='delta', \
+            default=0.05, help='temperature decrement factor')
+    parser.add_option('-k', '--transitions', type='int', dest='noTransitions',\
+            default=10000, help='the length of the k-th Markov chain')
+
+    (options, args) = parser.parse_args()
+    
+    Microstate.chain = Chain(options.sequence)
+
+if __name__ == '__main__':
+    main()
